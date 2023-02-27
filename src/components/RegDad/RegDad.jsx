99ab773e47ea0d17b7eb2d5bae35e74a595587e9
salomoncjl <115399253+salomoncjl@users.noreply.github.com>
2023-02-27 12:37:48 -0300

    M-02-Terminado (#2)

---
+++ src/components/RegDad/RegDad.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './regdad.css'
+import RegLeft from '../RegLeft/RegLeft';
+import RegRight from '../RegRight/RegRight';
+
+export default function RegDad() {
+  return (
+    <div class="reg-dad">
+        <RegLeft/>
+        <RegRight/>
+
+    </div>
+  )
+}
---
+++ src/components/RegRight/RegRight.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './regright.css'
+
+export default function RegRight() {
+  return (
+    <div class="reg-right">
+    </div>
+  )
+}
